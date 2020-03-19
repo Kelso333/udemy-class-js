1bfeed0dcb618ca018d96a8ae84a55a08c1ced1b
Rosibel Cisneros <kelsocisneros@gmail.com>
2020-03-18 22:13:53 -0700

    using back ticks

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,4 +6,6 @@ result = (result + 10) * 3 / 2 - 1;
 
 let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
 
+// back ticks `...` work as strings, they give you access to a specific syntax ... to get a value you have to use ${insert your variable name here or an equation itself ..(e.g- 1+1)}
+
 outputResult(result, calculationDescription);
