478cc8f3683f74482b35a270b85d1888b3d18b9d
Rosibel Cisneros <kelsocisneros@gmail.com>
2020-03-18 22:01:20 -0700

    using const variables assigning it to a let variable; a copy, const variables in comparison to let variables

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,9 @@
-let result = 0;
+
+const defaultResult = 0;
+let result = defaultResult;
 
 result = (result + 10) * 3 / 2 - 1;
 
-let calculationDescription = '(' + result + ' + 10) * 3 / 2 - 1';
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
 
 outputResult(result, calculationDescription);
