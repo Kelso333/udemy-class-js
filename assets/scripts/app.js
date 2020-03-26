484e20ac4b4a8a0d3b9a0839e92e89103aa3f3c0
Rosibel Cisneros <kelsocisneros@gmail.com>
2020-03-25 20:55:09 -0700

    added function parseInt to have the user input value become a number

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,13 +4,13 @@
 // back ticks `...` work as strings, they give you access to a specific syntax ${} ... to get a value you have to use ${insert your variable name here or an equation itself ...(e.g - ${1+1})
 
 const defaultResult = 0;
-let result = defaultResult;
+let currentResult = defaultResult;
 
 // a function is a statement 
 
 function add() {
-  result = result + userInput.value;
-  outputResult(result, '');
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, '');
 } 
 
 addBtn.addEventListener('click', add);
